e93858b039a9b68bf424aee8913d91b007af0be0
kalyan-nagakumar <nagakumar.work@gmai.com>
2023-12-05 11:34:11 -0600

    Initial Commit

---
+++ WeatherApp-Nagakumar/WeatherApp_NagakumarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_NagakumarApp.swift
+//  WeatherApp-Nagakumar
+//
+//  Created by Kalyan Vajrala on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_NagakumarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
